d7e0334a0d89da7890223b3b9b7d27a7082e42a8
Dipika K.C <dipikak0323@gmail.com>
2026-06-04 22:25:23 +0545

    feat: Subscription and Trial

--- apps/api/src/controllers/agency.controller.ts
+++ apps/api/src/controllers/agency.controller.ts
@@ -1,12 +1,46 @@
-import { Request, Response } from "express";
-import { createAgency } from "../services/agency.service";
+import type { Request, Response } from "express";
+import { agencySubscription, createAgency, getSubscription } from "../services/agency.service";
 
 export const registerAcency = async (req: Request, res: Response) => {
     try {
-        const result = await createAgency(req.body);
+        const newAgency = await createAgency(req.body);
         res.status(201).json({
             status: "success",
-            data: result
+            data: newAgency
+        });
+    } catch (err: any) {
+        res.status(err.status || 500).json({
+            status: "error",
+            message: err.message
+        });
+    }
+};
+
+export const getSubscriptionTiers = async (req: Request, res: Response) => {
+    try {
+        const tiers = await getSubscription();
+        res.status(200).json({
+            status: "success",
+            data: tiers
+        });
+    } catch (err: any) {
+        res.status(err.status || 500).json({
+            status: "error",
+            message: err.message
+        });
+    }
+};
+
+export const updateAgencySubscription = async (req: any, res: Response) => {
+    try {
+        const agencyId = req.agencyUser.id;
+        const { tier } = req.body;
+
+        const result = await agencySubscription(agencyId, tier);
+
+        return res.status(200).json({
+            success: true,
+            data: result,
         });
     } catch (err: unknown) {
         const error = err as { status?: number; message?: string };
